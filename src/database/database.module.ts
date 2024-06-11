f1cb27fcd29d20d616b69733d3aa648af4963e1a
Denys Mitiachkin <denismityachkin@gmail.com>
2024-06-12 00:36:10 +0300

    fix config, remove unused

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://root:Nd1MKM0YXM2cdjW9@tm-cluster.kkfs3yn.mongodb.net/?retryWrites=true&w=majority&appName=tm-cluster',
-    ),
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class DatabaseModule {}
